719c1027f4aa26314b701b3e8ade95001d4761e8
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-10-08 17:00:23 +0300

    Added user Prompt, and Function to insert the Divs
    the Prompt allows user to input whatever value they want.
    the function creates and adds the divs to the dom

---
+++ Javascript.js
@@ -0,0 +1,37 @@
+//take input from customer
+
+
+let fatherDiv = document.querySelector('.container');
+/*
+let childDiv = document.createElement('div');
+
+childDiv.classList.add('child');
+childDiv.style.border = "solid 2px black";
+childDiv.style.width = "40px";
+childDiv.style.height = "40px";
+
+fatherDiv.appendChild(childDiv);
+
+*/
+
+
+
+function addDivs(userInput){
+    for(let i=0; i<userInput; i++){
+        
+        for(let j=0; j<userInput; j++){
+
+            let childDiv = document.createElement('div');
+            childDiv.classList.add('child');
+            childDiv.style.border = "solid 2px black";
+            childDiv.style.width = childDivMeasurement + "px";
+            childDiv.style.height = childDivMeasurement + "px";
+
+            fatherDiv.appendChild(childDiv);
+        }
+    }
+}
+
+let userInput = parseInt(prompt("Please Enter Value: "));
+let childDivMeasurement = 880/userInput;
+addDivs(userInput);
