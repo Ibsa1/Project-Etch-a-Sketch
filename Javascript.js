3cdbddb1e17ee137487c7b705871ee1f7445519d
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-10-10 13:08:22 +0300

    Added Opacity on addDivs js function but not

--- Javascript.js
+++ Javascript.js
@@ -3,18 +3,36 @@ let fatherDiv = document.querySelector('.container');
 
 
 function addDivs(userInput){
-    for(let i=0; i<userInput; i++){
-        
-        for(let j=0; j<userInput; j++){
+    
+    for(let i=1; i<=userInput; i++){
+        let calculatedOpacity = 0.09;
+        for(let j=1; j<=userInput; j++){
 
             let childDiv = document.createElement('div');
             childDiv.classList.add('child');
             childDiv.style.border = "solid 2px black";
             childDiv.style.width = childDivMeasurement + "px";
             childDiv.style.height = childDivMeasurement + "px";
+            //childDiv.textContent = calculatedOpacity;
+            childDiv.textContent = calculatedOpacity.toFixed(1); // Display the opacity value
+            
+            childDiv.addEventListener("mouseover", function() {
+                childDiv.style.backgroundColor = "yellow"; // Change to the desired hover background color
+                childDiv.style.opacity = calculatedOpacity;
+            });
+            
+            childDiv.addEventListener("mouseout", function() {
+                childDiv.style.opacity = 1; // Reset opacity
+                childDiv.style.backgroundColor = "white"; // Reset to original background color
+            });
+            
 
             fatherDiv.appendChild(childDiv);
+
+            //calculatedOpacity += 0.1;
+            calculatedOpacity = Math.min(calculatedOpacity + 0.1, 1);
         }
+        
     }
 }
 
@@ -30,7 +48,7 @@ function noOfSquareInput(){
 
     let userInput = parseInt(prompt("Please Enter Value: "));
 
-    while(userInput>100){
+    while(userInput>100 || userInput<1){
         userInput = parseInt(prompt("Please Enter Value: "));
     }
 
