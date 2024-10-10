b20e0aacb9033e48889e406bc2f76492d447e2f9
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-10-10 20:02:57 +0300

    Added Erase button.

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 
 let fatherDiv = document.querySelector('.container');
-
+//fatherDiv.style.border = "solid 1px gray";
 
 function addDivs(userInput){
     
+    fatherDiv.style.border = "solid 1px gray";
+
     for(let i=1; i<=userInput; i++){
         let calculatedOpacity = 0.09;
         for(let j=1; j<=userInput; j++){
 
             let childDiv = document.createElement('div');
             childDiv.classList.add('child');
-            childDiv.style.border = "solid 2px black";
+            //childDiv.style.border = "solid 1px black";
             childDiv.style.width = childDivMeasurement + "px";
             childDiv.style.height = childDivMeasurement + "px";
             //childDiv.textContent = calculatedOpacity;
-            childDiv.textContent = calculatedOpacity.toFixed(1); // Display the opacity value
+            //childDiv.textContent = calculatedOpacity.toFixed(1); // Display the opacity value
             
             childDiv.addEventListener("mouseover", function() {
-                childDiv.style.backgroundColor = "yellow"; // Change to the desired hover background color
+                childDiv.style.backgroundColor = "black"; // Change to the desired hover background color
                 childDiv.style.opacity = calculatedOpacity;
             });
             
+            /*
             childDiv.addEventListener("mouseout", function() {
                 childDiv.style.opacity = 1; // Reset opacity
                 childDiv.style.backgroundColor = "white"; // Reset to original background color
-            });
+            }); */
             
 
             fatherDiv.appendChild(childDiv);
@@ -36,10 +39,19 @@ function addDivs(userInput){
     }
 }
 
+//No of Squares
 let isButtonClicked = false;
-
 const btn = document.querySelector(".noOfSquares");
 btn.addEventListener("click", noOfSquareInput);
+
+//Erase
+
+const eraseButton = document.querySelector(".erase");
+
+eraseButton.addEventListener("click", function() {
+    fatherDiv.style.border = "solid 1px white";
+    fatherDiv.replaceChildren(); // This will be executed when the erase button is clicked
+});
 
 
 let childDivMeasurement = 0;
